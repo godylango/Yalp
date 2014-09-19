c114195a3908960b726324cb3b244b3917529209
Dylan Clark <dylan@indiegogo.com>
2014-09-19 10:54:38 -0700

    Get Table View working.

---
+++ yalp/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SearchResultsViewController.swift
+//  yalp
+//
+//  Created by dylan on 9/19/14.
+//  Copyright (c) 2014 ClarkProvisions. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("SearchResult") as SearchResultTableViewCell
+        cell.searchResultLabel.text = "YOLO TONY \(indexPath.row)"
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ yalp/Views/SearchResultTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchResultTableViewCell.swift
+//  yalp
+//
+//  Created by dylan on 9/18/14.
+//  Copyright (c) 2014 ClarkProvisions. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var searchResultLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
